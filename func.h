e2d5f31109e72c693b66a8eb2d938f5b74ea6b49
SisAdminNsk <artemoktan777@gmail.com>
2023-04-18 21:36:36 +0700

    move files into folders

--- func.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <vector>
-#include "CircleClass.h"
-
-float CalculateCenterDistance(float x2, float x1, float y2, float y1);
-void WriteCircleIntersections(std::vector <CircleClass>& allCircles);

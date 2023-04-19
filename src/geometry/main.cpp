78fd3a35a67ea28f5ec11ed394377baa98b0ca00
SisAdminNsk <artemoktan777@gmail.com>
2023-04-19 16:36:09 +0700

    delete artifacts

--- src/geometry/main.cpp
+++ src/geometry/main.cpp
@@ -5,7 +5,7 @@
 #include <string> 
 int main() 
 {
-	std::string path = "data/WKT";
+	std::string path = "WKT";
 	AnalyseDataClass data;
 	std::map <std::string, std::vector<std::string>> parsedCircleData = data.TakeWktDataForCircle(path);
 

13b86c08c96683c161d45e8e90d1fc29b37c9184
SisAdminNsk <artemoktan777@gmail.com>
2023-04-27 18:11:45 +0700

    added tests of perimeter functions

--- test_src/test_lib/test.cpp
+++ test_src/test_lib/test.cpp
@@ -1,18 +1,39 @@
 #include <ctest.h>
-#include <func.h>// for math logic test
+#include <func.h>// for math logic group of  tests
 
 CTEST(MATH_LOGIC_TEST_suite, IsCalculatingDistanceTrue)
 {
     // When
-    const int result_1 = CalculateCenterDistance(10.5, 15, 15, 10);
-    const int result_2 = CalculateCenterDistance(100, 50, 10.5, 12);
-    const int result_3 = CalculateCenterDistance(0,0,0,0);
+    const float result_1 = round(CalculateCenterDistance(10.5, 15, 15, 10)*10000)/10000;
+    const float result_2 = round(CalculateCenterDistance(100, 50, 10.5, 12)*10000)/10000;
+    const float result_3 = round(CalculateCenterDistance(0,0,0,0)*10000)/10000;
     // Then
-    const int expected_1 = 6.72681;
-    const int expected_2 = 50.0225;
-    const int expected_3 = 0;
+    const float expected_1 = 6.7268;
+    const float expected_2 = 50.0225;
+    const float expected_3 = 0.f;
 
     ASSERT_DBL_NEAR(expected_1, result_1);
     ASSERT_DBL_NEAR(expected_2, result_2);
     ASSERT_DBL_NEAR(expected_3, result_3);
 }
+
+CTEST(MATH_LOGIC_TEST_suite,IsCalculatingPerimeterTrue)
+{   
+    // When
+    const float result_1 = round(CalculatePerimeter(10)*10000)/10000;
+    const float result_2 = round(CalculatePerimeter(15)*10000)/10000;
+    const float result_3 = round(CalculatePerimeter(0)*10000)/10000;
+    // Then
+    const float expected_1 = 62.8319f;
+    const float expected_2 = 94.2478f;
+    const float expected_3 = 0.f;
+
+    ASSERT_DBL_NEAR(expected_1, result_1);
+    ASSERT_DBL_NEAR(expected_2, result_2);
+    ASSERT_DBL_NEAR(expected_3, result_3);
+}
+
+//CTEST(MATH_LOGIC_TEST_suite,IsCalculatingAreaTrue)
+//{
+
+//}
